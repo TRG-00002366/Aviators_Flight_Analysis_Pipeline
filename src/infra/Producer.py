5cf201410506a757f395e913689667fcd0dd8caf
navdeepnatt41 <nattnavdeep41@gmail.com>
2026-03-11 23:20:21 -0500

    10. Reworked Producer to be a bit cleaner and more modular. Also removed uuid for flight_number, instead used custom flight_number generator in flight.py

--- src/infra/Producer.py
+++ src/infra/Producer.py
@@ -13,49 +13,46 @@ from aiokafka import AIOKafkaProducer
 from src.domain.flight import Flight
 from src.additionals.logger import LOGGER_NAME
 
-
-
 logger = logging.getLogger(LOGGER_NAME)
 
-async def flight_emitter_task():
-    logger.info("Beginning flight creation")
-    flight: Flight = Flight.new_random_flight(datetime.now(), str(uuid.uuid1(node=None, clock_seq=None)))
-    logger.info("New random flight created")
+async def flight_task(producer: AIOKafkaProducer) -> None:
+    flight: Flight = Flight.new_random_flight(datetime.now())
+    logger.info(f"Emitting flight {flight.flight_number} ")
     
-    logger.info("Starting AIOKakfaProducer...")
-    producer: AIOKafkaProducer = AIOKafkaProducer(
-        bootstrap_servers='localhost:9092'
-    )
+    for event in flight.flight_events:
+        logger.debug(f"Flight {event.flight_number} -> lat={event.location.latitude}, lon={event.location.longitude}")
+        await producer.send("flight_events", event.model_dump_json().encode("utf-8"))
+        await asyncio.sleep(1)
+
+    logger.debug(f"Flight {flight.flight_number} fully emitted")
+
+def create_task(producer: AIOKafkaProducer, tasks: set) -> None:
+    task = asyncio.create_task(flight_task(producer))
+    tasks.add(task)
+    task.add_done_callback(tasks.discard)
+    logger.debug(f"Task {task.get_name()} started")
+
+async def main():
+    logger.info("Producer connecting...")
+    producer: AIOKafkaProducer = AIOKafkaProducer(bootstrap_servers='localhost:9092')
     await producer.start()
-    logger.info("AIOKafkaProducer starting")
-    
-    try: 
-        for event in flight.flight_events:
-            # For now, we'll just log as a sign we've emitted
-            logger.info(f"Sending flight event {event.flight_number} to Kafka Topic {"flight_topic"}")
-            await producer.send("flight_events", event.model_dump_json().encode("utf-8"))
-            await asyncio.sleep(1)
-    finally:
-        await producer.stop()
-        
-async def a_main():
-    logger.info("Starting data generation...")
+    logger.info("Producer connected")
+
     tasks = set()
     N = 4
+
     for i in range(0, N):
-        logger.debug("Creating and adding async tasks to tasks")
-        task = asyncio.create_task(flight_emitter_task())
-        tasks.add(task)
-        task.add_done_callback(tasks.discard)
-    logger.info("Tasks added")
-    while True:
-        if len(tasks) < N:
-           logger.info("A task has finished, adding new task for flight generation and emission")
-           for i in range(N - len(tasks)):
-               task = asyncio.create_task(flight_emitter_task())
-               tasks.add(task)
-               task.add_done_callback(tasks.discard)
-               logger.info("Task added and started")
-        await asyncio.sleep(0.1)
-               
-asyncio.run(a_main())
+        create_task(producer, tasks)
+    logger.info(f"Started {N} workers")
+
+    try:
+        while True:
+            while len(tasks) < N:
+                create_task(producer, tasks) 
+            await asyncio.sleep(0.1)
+    finally:
+        logger.info("Shutting down producer...")
+        await producer.stop()
+        logger.info("Producer stopped")
+
+asyncio.run(main())
