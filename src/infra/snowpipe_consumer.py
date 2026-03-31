d7432c5b998f3f124add1908688de1005931dddd
Navdeep Natt <nattnavdeep41@gmail.com>
2026-03-31 03:44:10 -0500

    Success! We now have a streaming consumer that streams flight events to Snowflake! Onto to dbt transforms to silver and gold!

--- src/infra/snowpipe_consumer.py
+++ src/infra/snowpipe_consumer.py
@@ -1,34 +1,41 @@
 """
 Responsible for pulling flight events from the flight_events topic and sending them off to Snowflake's DB.
 """
-import time
-
+import json
 from snowflake.ingest.streaming import StreamingIngestClient
 from kafka import KafkaConsumer
 
 if __name__ == "__main__":
+    # ADDED: value_deserializer to automatically parse Kafka bytes into a Python dict
     kafka_consumer: KafkaConsumer = KafkaConsumer(
         'flight_events',
         bootstrap_servers="kafka:29092",
-        client_id="Snowpipe_Consumer"
+        client_id="Snowpipe_Consumer",
+        value_deserializer=lambda m: json.loads(m.decode('utf-8'))
     )
     
     with StreamingIngestClient(
         client_name="I_AM_THE_CLIENT",
         db_name="FLIGHT_EVENTS_DB",
         schema_name="BRONZE",
-        pipe_name="FLIGHT_EVENTS_PIPE",
+        pipe_name="RAW_FLIGHT_EVENTS-STREAMING",
         profile_json="profile.json"
     ) as client:
         
         with client.open_channel("A_CHANNEL")[0] as channel:
-            counter: int = 0
+            # counter: int = 0
             for message in kafka_consumer:
-                if counter < 10:
+                # if counter < 10:
+                    # FIXED: 1. Uppercase key to match Snowflake
+                    # FIXED: 2. message.value instead of message
+                    # FIXED: 3. Added the required string token (message.offset)
                     channel.append_row(
                         {
-                            "flight_event_data": message
-                        }
+                            "FLIGHT_EVENT_DATA": message.value
+                        },
+                        str(message.offset)
                     )
-                else:
-                    break
+                #     counter += 1
+                    # print(f"Streamed message {counter}/10")
+                    # else:
+                    # break
